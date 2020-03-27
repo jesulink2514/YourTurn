4e256f6bbbca06fceac17c38b3c8f9d28176d010
jesulink2514 <jesus.angulo@outlook.com>
2020-03-26 22:58:57 -0500

    protect api

--- Techies.YourTurn.Api/Controllers/WeatherForecastController.cs
+++ Techies.YourTurn.Api/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace Techies.YourTurn.Api.Controllers
 {
     [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- Techies.YourTurn.Api/Startup.cs
+++ Techies.YourTurn.Api/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Techies.YourTurn.Api
 {
@@ -27,10 +21,26 @@ namespace Techies.YourTurn.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.BearerAuthenticationScheme)
-                .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(jwtOptions =>
+                {
+                    jwtOptions.Authority = $"https://{Configuration["AzureAdB2C:Domain"]}/{Configuration["AzureAdB2C:Tenant"]}/v2.0/";
+                    jwtOptions.Audience = Configuration["AzureAdB2C:ClientId"];
+                    jwtOptions.MetadataAddress =
+                        $"https://{Configuration["AzureAdB2C:Domain"]}/{Configuration["AzureAdB2C:Tenant"]}/v2.0/.well-known/openid-configuration?p={Configuration["AzureAdB2C:Policy"]}";
+                });
 
-            services.AddControllers();
+            services.AddControllers(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireClaim("http://schemas.microsoft.com/identity/claims/scope", "api")
+                    .Build();
+
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
